c7f43cd0c85ed72b33243b4b0ae657c69736a637
Tanuj Bhatia <tj3895@gmail.com>
2014-06-27 00:53:58 +0530

    tv3 added

--- beatblender/src/main/java/com/beatblender/beatblender/MainActivity.java
+++ beatblender/src/main/java/com/beatblender/beatblender/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView;
+        TextView tv3;
 
     }
 
