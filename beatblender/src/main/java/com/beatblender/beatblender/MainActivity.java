139d186ba862026f3b21b333998ae33560868e85
Tanuj Bhatia <tj3895@gmail.com>
2014-06-27 00:49:42 +0530

    my first commit

--- beatblender/src/main/java/com/beatblender/beatblender/MainActivity.java
+++ beatblender/src/main/java/com/beatblender/beatblender/MainActivity.java
@@ -12,7 +12,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv;
+        TextView textView;
+
     }
 
 
